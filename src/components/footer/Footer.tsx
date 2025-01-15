b3c09efcd418f56dc1537d603dbffacbbefe2a0a
TiihAlves <tiih.alves1@gmail.com>
2025-01-15 14:41:58 -0300

    fix footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,13 +9,13 @@ function Footer() {
             <div className="flex justify-center bg-gradient-to-r from-blue-500 to-cyan-500 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
+                            Blog Pessoal Tiago Alves | Copyright: {data}
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+                        <LinkedinLogo size={48} weight='bold' className='bg-transparent hover:size-3/4' />
+                        <InstagramLogo size={48} weight='bold' className='bg-transparent hover:size-3/4'/>
+                        <FacebookLogo size={48} weight='bold' className='bg-transparent hover:size-3/4' />
                     </div>
                 </div>
             </div>
